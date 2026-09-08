35499f501580252ea294fac67cd4715885917430
aamaury <amaury.aune@ynov.com>
2026-09-08 15:34:39 +0200

    [FIX] fusion #clean des routes en dur

--- R00t_Override/app/main.py
+++ R00t_Override/app/main.py
@@ -9,19 +9,7 @@ app.include_router(rooms.router)
 
 @app.get("/")
 def read_root():
-    return {"status": "ok", "message": "Environnement Conda prêt pour l'Escape Game R00T override !"}
+    return {"status": "ok", "message": "Environnement Conda prêt pour l'Escape Game R00T override Routes tested!"}
 
 # salle_1 = GameElement(1, "pare-Feu", "Obtenez la Clé de Validation Externe")
 
-salles = {
-    1: {"name": "Pare-Feu", "bio": "Obtenez la Clé de Validation Externe"},
-    2: {"name": "Proxy", "bio": "Décrochez les Privilèges Intermédiaires"}
-}
-
-@app.get("/rooms")
-def get_rooms():
-    return list(salles.keys())
-
-@app.get("/rooms/{salle_id}")
-def get_room(salle_id):
-    return salle_id, salles.get(int(salle_id))
--- R00t_Override/app/routers/rooms.py
+++ R00t_Override/app/routers/rooms.py
@@ -3,10 +3,23 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/rooms", tags=["rooms"])
 
 
-@router.get("/1")
-def get_salle1():
-    return {"message": "Tu es bien dans la salle 1"}
+# @router.get("/1")
+# def get_salle1():
+#     return {"message": "Tu es bien dans la salle 1"}
 
-@router.get("/2")
-def get_salle2():
-    return {"message": "Tu es bien dans la salle 2"}
+# @router.get("/2")
+# def get_salle2():
+#     return {"message": "Tu es bien dans la salle 2"}
+
+salles = {
+    1: {"name": "Pare-Feu", "bio": "Obtenez la Clé de Validation Externe"},
+    2: {"name": "Proxy", "bio": "Décrochez les Privilèges Intermédiaires"}
+}
+
+@router.get("/")
+def get_rooms():
+    return list(salles.keys())
+
+@router.get("/{salle_id}")
+def get_room(salle_id):
+    return salle_id, salles.get(int(salle_id))
