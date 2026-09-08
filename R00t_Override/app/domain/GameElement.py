6809bb747e3612692b6fa6c3d860b217788eae32
aamaury <amaury.aune@ynov.com>
2026-09-08 15:20:33 +0200

    [ADD] debut class Element + routes basiques main

---
+++ R00t_Override/app/domain/GameElement.py
@@ -0,0 +1,15 @@
+class GameElement:
+
+	def __init__(self, id: int, name: str, bio: str):
+		self.id = id
+		self.name = name
+		self.bio = bio
+
+
+# class Salle(GameElement):
+
+# 	id = 1
+# 	name = "Pare-feu"
+# 	bio = "Obtenez la Clé de Validation Externe"
+
+# salle_1 = GameElement(1, "Pare-feu", "Obtenez la Clé de Validation Externe")
--- R00t_Override/app/main.py
+++ R00t_Override/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+# from app.domain.GameElement import GameElement
 
 app = FastAPI(title="RootOverride API Test")
 
 @app.get("/")
 def read_root():
     return {"status": "ok", "message": "Environnement Conda prêt pour l'Escape Game R00T override !"}
+
+# salle_1 = GameElement(1, "pare-Feu", "Obtenez la Clé de Validation Externe")
+
+salles = {
+    1: {"name": "Pare-Feu", "bio": "Obtenez la Clé de Validation Externe"},
+    2: {"name": "Proxy", "bio": "Décrochez les Privilèges Intermédiaires"}
+}
+
+@app.get("/rooms")
+def get_rooms():
+    return list(salles.keys())
+
+@app.get("/rooms/{salle_id}")
+def get_room(salle_id):
+    return salle_id, salles.get(int(salle_id))
